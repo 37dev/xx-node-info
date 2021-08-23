1bc059be7530aaa9a023a2dcfb41131998edeec7
37dev <markthedev37@gmail.com>
2021-08-22 22:01:56 -0300

    Add NodeInfo model

---
+++ nodeinfo/managers.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class NodeInfoManager(models.Manager):
+    def update_or_create_from_event_data(self, event_data):
+        for node_id, status in event_data.items():
+            node_info, _ = self.update_or_create(
+                node_id=node_id, defaults={
+                    "status": status
+                }
+            )
--- nodeinfo/models.py
+++ nodeinfo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from nodeinfo.managers import NodeInfoManager
+
+
+class NodeInfo(models.Model):
+    name = models.CharField(max_length=200)
+    node_id = models.CharField(max_length=200)
+    group = models.CharField(max_length=200)
+    application_id = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    round_failure_avg = models.DecimalField(max_digits=3, decimal_places=2, null=True)
+    status = models.CharField(max_length=200)
+    team = models.CharField(max_length=200)
+    uptime = models.DecimalField(max_digits=3, decimal_places=2, null=True)
+    application_url = models.CharField(max_length=200)
+
+    objects = NodeInfoManager()
+
+    def __str__(self):
+        return self.node_id
