1dd53d1e126bf85697d409de727532e7ef70ecb9
37dev <markthedev37@gmail.com>
2021-08-23 23:53:51 -0300

    Update static texts

---
+++ tgbot/handlers/static_text.py
@@ -0,0 +1,18 @@
+start_text = 'Welcome {first_name}! Use "`/subscribe node_id`"' \
+             ' to receive status updates about a node.'
+
+already_started_text = "You have started the bot already."
+
+node_status_text = "{emoji}{emoji}{emoji}\n" \
+                   "Node status has changed: {status}\n" \
+                   "Node ID: {node_id}"
+
+node_subscribe_text = "You have successfully subscribed to the node. \nCurrent node status: {}"
+
+node_already_subscribed_text = "You are already subscribed to this node."
+
+node_unsubscribe_text = "You have successfully unsubscribed from the node."
+
+node_already_unsubscribed_text = "You are already unsubscribed of this node."
+
+node_id_does_not_exist_text = "Node ID does not exist. Please, insert a valid one."
